b680e6259e6ea97076b76968bdcd355045451d87
turuslan <turuslan.devbox@gmail.com>
2024-11-15 11:18:00 +0500

    graypaper references
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- test-vectors/common.hpp
+++ test-vectors/common.hpp
@@ -33,8 +33,8 @@ namespace jam {
     return s[i % s.size()];
   }
 
-  // [GP 0.4.3 3.7.2]
-  // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/notation.tex#L132
+  // [GP 0.4.5 3.7.2]
+  // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/notation.tex#L132
   template <size_t X, size_t Y>
   qtils::BytesN<X + Y> frown(
       const qtils::BytesN<X> &x, const qtils::BytesN<Y> &y) {
@@ -71,8 +71,8 @@ namespace jam {
     return Blake::hash(m);
   }
 
-  // [GP 0.4.3 3.8.1]
-  // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/notation.tex#L153
+  // [GP 0.4.5 3.8.1]
+  // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/notation.tex#L153
   inline auto mathcal_H_K(qtils::BytesIn m) {
     return Keccak::hash(m);
   }
--- test-vectors/history/history.hpp
+++ test-vectors/history/history.hpp
@@ -17,12 +17,12 @@ namespace jam::history {
   /**
    * The size of recent history, in blocks
    */
-  // [GP 0.4.3 I.4.4]
-  // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/definitions.tex#L266
+  // [GP 0.4.5 I.4.4]
+  // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/definitions.tex#L266
   constexpr uint32_t H = 8;
 
-  // [GP 0.4.3 E.2 327]
-  // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/merklization.tex#L205
+  // [GP 0.4.5 E.2 333]
+  // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/merklization.tex#L205
   inline types::Mmr mathcal_A(types::Mmr r, types::Hash l) {
     for (size_t n = 0; n < r.peaks.size(); ++n) {
       if (not r.peaks[n]) {
@@ -41,18 +41,18 @@ namespace jam::history {
    */
   inline std::pair<types::State, types::Output> transition(
       const types::State &state, const types::Input &input) {
-    // [GP 0.4.3 7 84]
-    // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/recent_history.tex#L32
+    // [GP 0.4.5 7 84]
+    // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/recent_history.tex#L32
     std::vector beta_tick(state.beta.size() >= H ? std::next(state.beta.begin())
                                                  : state.beta.begin(),
         state.beta.end());
-    // [GP 0.4.3 7 82]
-    // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/recent_history.tex#L12
+    // [GP 0.4.5 7 82]
+    // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/recent_history.tex#L12
     if (not beta_tick.empty()) {
       beta_tick.back().state_root = input.parent_state_root;
     }
-    // [GP 0.4.3 7 83]
-    // https://github.com/gavofyork/graypaper/blob/v0.4.3/text/recent_history.tex#L20
+    // [GP 0.4.5 7 83]
+    // https://github.com/gavofyork/graypaper/blob/v0.4.5/text/recent_history.tex#L20
     types::Mmr mmr;
     if (not state.beta.empty()) {
       mmr = state.beta.back().mmr;
