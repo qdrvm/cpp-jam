b76364f61d53aaffc0a8ec3d85fd1120628224e2
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2024-11-21 16:26:17 +0800

    feature: Disputes jam-test-vector

--- test-vectors/disputes/disputes.test.cpp
+++ test-vectors/disputes/disputes.test.cpp
@@ -4,11 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <test-vectors/disputes/disputes.hpp>
+#include <test-vectors/disputes/types.diff.hpp>
 #include <test-vectors/disputes/vectors.hpp>
 
 GTEST_VECTORS(Disputes, jam::test_vectors_disputes::Vectors);
 
-/**
- * Check python generated scale encoding/decoding against test vectors.
- */
-GTEST_VECTORS_TEST_REENCODE(Disputes, jam::disputes);
+GTEST_VECTORS_TEST_TRANSITION(Disputes, jam::disputes);
