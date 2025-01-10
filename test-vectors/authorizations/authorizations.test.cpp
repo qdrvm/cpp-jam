7c57b3749805fb1377579258f631a2565ad92e99
Dmitriy Khaustov <khaustov.dm@gmail.com>
2025-01-11 03:45:09 +0800

    Test-Vector 'Authorizations' (#18)
    * feature: authorizations
    
    * refactor: remove config-arg from asn1.py
    
    * fix: remove debugging code
    
    * feature: test-vector authorizations
    
    * update: vcpkg-overlays
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    * fix: review issues
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    * refactor: removed boost::variant support
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    * fix: CI on Ubuntu
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    ---------
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

---
+++ test-vectors/authorizations/authorizations.test.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <test-vectors/authorizations/authorizations-diff.hpp>
+#include <test-vectors/authorizations/authorizations.hpp>
+#include <test-vectors/authorizations/vectors.hpp>
+
+GTEST_VECTORS(Authorizations, authorizations);
+
+GTEST_VECTORS_TEST_TRANSITION(Authorizations, authorizations);
