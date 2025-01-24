3d0ebc248242ba22b0d5432be7681a25ec0f8a6e
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-01-25 02:06:46 +0800

    no conf types scale
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- test-vectors/config-types.hpp
+++ test-vectors/config-types.hpp
@@ -8,11 +8,14 @@
 
 #include <vector>
 
+#include <test-vectors/config.hpp>
+#include <scale/scale.hpp>
+
 namespace jam {
   template <typename T, typename ConfigField>
   class ConfigVec : public std::vector<T> {
    public:
-    ConfigVec() = default; // To make non-aggregate
+    ConfigVec() = default;  // To make non-aggregate
 
     static size_t configSize(const auto &config) {
       return config.get(ConfigField{});
@@ -23,5 +26,28 @@ namespace jam {
     using std::vector<T>::vector;
     using std::vector<T>::insert;
     using std::vector<T>::emplace;
+
+   public:
+    friend scale::ScaleEncoderStream &operator<<(scale::ScaleEncoderStream &s,
+                                                 const ConfigVec &v) {
+      const auto &config = s.getConfig<test_vectors::Config>();
+      auto n = v.configSize(config);
+      assert(v.size() == n);
+      for (auto &item : v) {
+        s << item;
+      }
+      return s;
+    }
+
+    friend scale::ScaleDecoderStream &operator>>(scale::ScaleDecoderStream &s,
+                                                 ConfigVec &v) {
+      const auto &config = s.getConfig<test_vectors::Config>();
+      auto n = v.configSize(config);
+      v.resize(n);
+      for (auto &item : v) {
+        s >> item;
+      }
+      return s;
+    }
   };
 }  // namespace jam
