5c0f4445d4b72dfb0449cd309f84b8d9e9aad4c9
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-01-25 01:21:58 +0800

    draft
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- src_/TODO_scale/aggregate.hpp
+++ src_/TODO_scale/aggregate.hpp
@@ -131,9 +131,9 @@ namespace scale::detail {
       auto &[v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20] = v;
       return f(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20);
     } else if constexpr (N <= MAX_FIELD_NUM) {
-      BOOST_STATIC_ASSERT_MSG("Inconsistent value of MAX_FIELD_NUM");
+      static_assert(!"Inconsistent value of MAX_FIELD_NUM");
     } else {
-      BOOST_STATIC_ASSERT_MSG("No code for cover aggregate with such big amount of fields");
+      static_assert(!"No code for cover aggregate with such big amount of fields");
     }
     // clang-format on
   }
