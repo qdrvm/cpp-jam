f7e1c19506314529523244eb56230eb4219bd77d
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-02-01 01:24:51 +0800

    fix: get_version
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -54,31 +54,28 @@ SANITIZED=false
 [ "$#" -gt 0 ] && [ "$1" = "--sanitized" ] && SANITIZED=true
 
 if [ -x "$(command -v git)" ] && [ -d "$(git rev-parse --git-dir 2>/dev/null)" ]; then
-   HEAD=$(git rev-parse --short HEAD)
-    COMMON=$(git merge-base HEAD "$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')")
+  HEAD=$(git rev-parse --short HEAD)
 
-    if ! git tag | grep -q .; then
-      ROOT_COMMIT=$(git rev-list --max-parents=0 HEAD)
-      if [ -n "$ROOT_COMMIT" ]; then
-          git tag _init "$ROOT_COMMIT"
-      else
-          printf "unknown"
-          exit 1
-      fi
-    fi
+  MAIN_BRANCH=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+  COMMON=$(git merge-base HEAD "$MAIN_BRANCH")
 
-    DESCR=$(git describe --tags --long "${COMMON}" || echo "")
-    [ -z "$DESCR" ] && DESCR="$HEAD-0-g$HEAD"
+  if ! git tag | grep -q .; then
+    ROOT_COMMIT=$(git rev-list --max-parents=0 HEAD || echo "")
+    [ -n "$ROOT_COMMIT" ] && ! git tag | grep -q "_init" && git tag _init "$ROOT_COMMIT"
+  fi
 
-    TAG_IN_MASTER=$(echo "$DESCR" | sed -E "s/v?(.*)-([0-9]+)-g[a-f0-9]+/\1/")
-    TAG_TO_FORK_DISTANCE=$(echo "$DESCR" | sed -E "s/v?(.*)-([0-9]+)-g[a-f0-9]+/\2/")
+  DESCR=$(git describe --tags --long "${COMMON}" || echo "")
+  [ -z "$DESCR" ] && DESCR="$HEAD-0-g$HEAD"
 
-    BRANCH=$(git branch --show-current || echo "$HEAD")
-    FORK_TO_HEAD_DISTANCE=$(git rev-list --count "${COMMON}..HEAD")
+  TAG_IN_MASTER=$(echo "$DESCR" | sed -E "s/v?(.*)-([0-9]+)-g[a-f0-9]+/\1/")
+  TAG_TO_FORK_DISTANCE=$(echo "$DESCR" | sed -E "s/v?(.*)-([0-9]+)-g[a-f0-9]+/\2/")
 
-    RESULT=$TAG_IN_MASTER
-    [ "$TAG_TO_FORK_DISTANCE" != "0" ] && RESULT="$RESULT-$TAG_TO_FORK_DISTANCE"
-    [ "$BRANCH" != "$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')" ] && RESULT="$RESULT-$BRANCH-$FORK_TO_HEAD_DISTANCE-$HEAD"
+  BRANCH=$(git branch --show-current || echo "$HEAD")
+  FORK_TO_HEAD_DISTANCE=$(git rev-list --count "${COMMON}..HEAD")
+
+  RESULT=$TAG_IN_MASTER
+  [ "$TAG_TO_FORK_DISTANCE" != "0" ] && RESULT="$RESULT-$TAG_TO_FORK_DISTANCE"
+  [ "$BRANCH" != "$MAIN_BRANCH" ] && RESULT="$RESULT-$BRANCH-$FORK_TO_HEAD_DISTANCE-$HEAD"
 
   git diff --quiet || DIRTY="-dirty"
   RESULT="$RESULT$DIRTY"
