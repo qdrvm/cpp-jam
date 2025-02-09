97702dbc3aa42b8699a3037edccc40ba478f063d
iceseer <iceseer@gmail.com>
2025-02-09 14:04:17 +0300

    loader base

--- src/loaders/loader.hpp
+++ src/loaders/loader.hpp
@@ -1,5 +1,26 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
+#pragma once
 
-struct Loader {
+#include "modules/module.hpp"
+#include "injector/node_injector.hpp"
 
+namespace jam::loaders {
+
+class Loader {
+public:
+    Loader(const Loader&) = delete;
+    Loader& operator=(const Loader&) = delete;
+
+    virtual ~Loader() = default;
+    virtual void start() = 0;
+
+protected:
+    injector::NodeInjector injector_;
+    modules::Module module_;
 };
+}
