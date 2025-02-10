d5cf1f07b44f530f1edbbf90cc2fd22cdceb2bd4
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-02-11 02:10:04 +0800

    update: scale
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/scale/portfile.cmake
+++ vcpkg-overlay/scale/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/scale-codec-cpp
-  REF c59aa2b98eec0b16a6f224740d9c4c0de616818e
-  SHA512 a4e3f18e8ade1e18fdd2df0791a47c62f856f0ea5dde663fd00c82dc71302e21b7fc99fd29392cd6f131d7ed263eafebccd2311c4f977297174b6332891a938e
+  REF 09f1ce9ffa75a50d48ea664757506ba7bd08d5c9
+  SHA512 3197dde041e8e1df2f98c413f7aab9999321b948128b8bea2f45b3a648fbedc45b0ddcc93779606f09bf813cb59dac71e4ad2426ce4c0e19f73b7d8e1aa8b454
 )
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
