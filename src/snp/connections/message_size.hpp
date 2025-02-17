b12bdcd18f54c143cd8976ff13594cf21be1d6ef
turuslan <turuslan.devbox@gmail.com>
2025-02-17 12:43:53 +0500

    numeric limits
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- src/snp/connections/message_size.hpp
+++ src/snp/connections/message_size.hpp
@@ -7,9 +7,11 @@
 #pragma once
 
 #include <cstdint>
+#include <limits>
 
 namespace jam::snp {
   // https://github.com/zdave-parity/jam-np/blob/5d374b53578cdd93646e3ee19e2b19ea132317b8/simple.md?plain=1#L109-L111
   using MessageSize = uint32_t;
-  constexpr MessageSize kMessageSizeMax = UINT32_MAX;
+  constexpr MessageSize kMessageSizeMax =
+      std::numeric_limits<MessageSize>::max();
 }  // namespace jam::snp
