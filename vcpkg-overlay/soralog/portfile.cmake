8db4675fa8a9420b28727a08b35677d2c31c1a15
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-03-05 02:58:46 +0800

    update: soralog
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/soralog/portfile.cmake
+++ vcpkg-overlay/soralog/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO xDimon/soralog
-  REF 4dfffd3d949b1c16a04db2e5756555a4031732f7
-  SHA512 4a8f6066433e6bde504454ea256915e8e6975c060eabdaebd031df409d348b8c22ccd716ec11c4466b0f4322b1f2524350dcc77c3615a8511cba9886f88e7260
+  REPO qdrvm/soralog
+  REF refs/tags/v0.2.5
+  SHA512 ce98f52bbca0ee81c672b5a7fee5846f47c39eed4a9dfdeb91a6670c3cbfc2f96df23cf87f84216847d20be7bc52e54fa98ade0b5b7ca832d4ac1a7560396449
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
