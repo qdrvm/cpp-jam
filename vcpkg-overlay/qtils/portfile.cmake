0a9707db495a74426ceb9b20a7cab83e39f18af0
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-03-18 16:56:16 +0800

    upd
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/qtils/portfile.cmake
+++ vcpkg-overlay/qtils/portfile.cmake
@@ -3,8 +3,8 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/qtils
 #  REF refs/tags/v0.1.0
-  REF 7fa095bed51f9c1b7d4a2c99552229df380373ea
-  SHA512 b03d6d324c28b57f20d23ae17be1e61fddf6d394c2b6fd5fff886d36018a12bd83593e25718e9d7f1ed88f8fb3b6ab71b5fb7c7b3f2f208493c8d16b22a0af2a
+  REF 163287d1e521788ebccc74bfcf8d1199cb25643b
+  SHA512 bd4d1f319dc2c810a2f6204d93f63c67f4b0139ddd9dedb9e374f8cb964a1ad7eb46f93f667b3b248abf8949b1f577e86c53a98aae04af926373c98c2c7582ee
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
