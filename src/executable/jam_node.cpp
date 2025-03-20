cde8655a085fb33ec222dc29d166a8b01ebf59c8
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-03-20 15:51:02 +0800

    feature: configurable base and module paths
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    # Conflicts:
    #	src/executable/jam_node.cpp

--- src/executable/jam_node.cpp
+++ src/executable/jam_node.cpp
@@ -41,11 +41,12 @@ namespace {
   int run_node(std::shared_ptr<LoggingSystem> logsys,
                std::shared_ptr<Configuration> appcfg) {
     auto injector = std::make_unique<NodeInjector>(logsys, appcfg);
+
     // Load modules
     std::deque<std::unique_ptr<jam::loaders::Loader>> loaders;
     {
       auto logger = logsys->getLogger("Modules", "jam");
-      const std::string path("modules");
+      const std::string path(appcfg->modulesDir());
 
       jam::modules::ModuleLoader module_loader(path);
       auto modules = module_loader.get_modules();
@@ -134,7 +135,7 @@ int main(int argc, const char **argv, const char **env) {
     std::make_shared<jam::log::LoggingSystem>(std::move(logging_system));
   });
 
-  // Parse CLI args for help, version and config
+  // Parse remaining args
   if (auto res = app_configurator->step2(); res.has_value()) {
     if (res.value()) {
       return EXIT_SUCCESS;
