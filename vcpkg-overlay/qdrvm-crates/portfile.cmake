5893a67ab0bfb1e4d1dabe859afbf3640812d9de
turuslan <turuslan.devbox@gmail.com>
2025-04-09 20:36:25 +0500

    update
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- vcpkg-overlay/qdrvm-crates/portfile.cmake
+++ vcpkg-overlay/qdrvm-crates/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/kagome-crates
-  REF a052aa3dc7d5233bc8f70ac39a9380f20bafe3a6
-  SHA512 5a8884668a80a3a9f1251f3dd48a8c7a252f9d893a1fe4c353cea0cc4343c7b25462d10573e7b9227584770cede6a6a2f31dbbdf5483616e0d24b83671db8269
+  REF 79271951cc3a90ceddd7a917f81b42ae43912c5a
+  SHA512 e8b7fe834c3e02b9ac70b87af08b13cd0aee98aac7e856dd5931981dbbdde2e1e388e9e3e479b9a2b155a62a487a13c0407333929a57517856d730bfa894dec4
 )
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS "-DQDRVM_BIND_CRATES=bandersnatch_vrfs;schnorrkel")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS "-DQDRVM_BIND_CRATES=bandersnatch_vrfs;schnorrkel;ark_ec_vrfs")
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "qdrvm-crates" CONFIG_PATH "lib/cmake/qdrvm-crates" NO_PREFIX_CORRECTION)
 
