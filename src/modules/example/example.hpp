e3e4642eccde3d82655d3f588c7188da45a1da6b
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-04-23 18:01:42 +0300

    draft
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- src/modules/example/example.hpp
+++ src/modules/example/example.hpp
@@ -6,46 +6,30 @@
 
 #pragma once
 
-#include <metrics/impl/session_impl.hpp>
-#include <modules/module_loader.hpp>
-#include <qtils/strict_sptr.hpp>
+#include <log/logger.hpp>
+#include <modules/example/interfaces.hpp>
 
 namespace jam::modules {
 
-  struct ExampleModuleLoader {
-    virtual ~ExampleModuleLoader() = default;
+  class ExampleModuleImpl final : public jam::modules::ExampleModule {
+    qtils::StrictSharedPtr<jam::modules::ExampleModuleLoader> loader_;
+    qtils::StrictSharedPtr<jam::log::LoggingSystem> logsys_;
+    jam::log::Logger logger_;
 
-    virtual void dispatch_request(std::shared_ptr<const std::string>) = 0;
-    virtual void dispatch_response(std::shared_ptr<const std::string>) = 0;
-    virtual void dispatch_notify(std::shared_ptr<const std::string>) = 0;
+   public:
+    ExampleModuleImpl(std::shared_ptr<jam::modules::ExampleModuleLoader> loader,
+                      std::shared_ptr<jam::log::LoggingSystem> logsys);
+
+    void on_loaded_success() override;
+
+    void on_loading_is_finished() override;
+
+    void on_request(std::shared_ptr<const std::string> s) override;
+
+    void on_response(std::shared_ptr<const std::string> s) override;
+
+    void on_notify(std::shared_ptr<const std::string> s) override;
   };
 
-  struct ExampleModule {
-    virtual ~ExampleModule() = default;
-    virtual void on_loaded_success() = 0;
-    virtual void on_loading_is_finished() = 0;
-
-    virtual void on_request(std::shared_ptr<const std::string>) = 0;
-    virtual void on_response(std::shared_ptr<const std::string>) = 0;
-    virtual void on_notify(std::shared_ptr<const std::string>) = 0;
-  };
-
-}  // namespace jam::modules
-
-// class BlockTree;
-
-namespace jam::modules {
-
-  // class ExampleModule : public Singleton<ExampleModule> {
-  //  public:
-  //   static std::shared_ptr<ExampleModule> instance;
-  //   CREATE_SHARED_METHOD(ExampleModule);
-
-  //   ExampleModule(qtils::StrictSharedPtr<ExampleModuleLoader> loader,
-  //                 qtils::StrictSharedPtr<log::LoggingSystem> logging_system);
-
-  //   qtils::StrictSharedPtr<ExampleModuleLoader> loader_;
-  //   log::Logger logger_;
-  // };
 
 }  // namespace jam::modules
