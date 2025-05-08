62b7fca5625936b2f4e6df09b90e706ff40639da
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-05-08 11:55:41 +0300

    feature: spaced storage
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- src/injector/node_injector.cpp
+++ src/injector/node_injector.cpp
@@ -29,7 +29,7 @@
 #include "se/impl/async_dispatcher_impl.hpp"
 #include "se/subscription.hpp"
 #include "storage/in_memory/in_memory_storage.hpp"
-
+#include "storage/in_memory/in_memory_spaced_storage.hpp"
 namespace {
   namespace di = boost::di;
   namespace fs = std::filesystem;
@@ -67,6 +67,7 @@ namespace {
           };
         }),
         di::bind<storage::BufferStorage>.to<storage::InMemoryStorage>(),
+        di::bind<storage::SpacedStorage>.to<storage::InMemorySpacedStorage>(),
 
         // user-defined overrides...
         std::forward<decltype(args)>(args)...);
