26162d70c62029cba2df31b32a76de7ffb0bc744
iceseer <iceseer@gmail.com>
2025-05-13 10:48:28 +0300

    build fix

--- vcpkg-overlay/kagome-crates/portfile.cmake
+++ vcpkg-overlay/kagome-crates/portfile.cmake
@@ -2,10 +2,14 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/kagome-crates
-  REF 9011440d56d51bb33e7c4c6a47e85314178ce87f
-  SHA512 f0b1b0e1ed6e20d149d5639e14c231a46f3cdaaa6bfa320bfd64e88c4ed0496fa4246c3135e0032647c05c2aaac02804b565fbeae34b1625771114c8b263920d
+  REF f84cc57eb6c0754753c05623c2f6831197596210
+  SHA512 6877b8ba3862a7e5ce7e5627fe1f50f3a7cdc06b1de12246af99eb756b47f114d4c0749d01aaa1ffb64d54645ee389e9535642d3de0b23a8c6d3ed286e1c8340
 )
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(
+  SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS
+    -DQDRVM_BIND_CRATES=pvm_bindings
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "kagome-crates")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
