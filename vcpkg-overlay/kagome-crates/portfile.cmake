901c8b6d56b2441d1d5c7fbc384544687c53d907
iceseer <iceseer@gmail.com>
2025-05-29 10:35:21 +0300

    [fix] kagome-crates in CI build [fix] init.sh and portfile [added] readme [added] Rust toolchain settings

--- vcpkg-overlay/kagome-crates/portfile.cmake
+++ vcpkg-overlay/kagome-crates/portfile.cmake
@@ -5,7 +5,10 @@ vcpkg_from_github(
   REF 9011440d56d51bb33e7c4c6a47e85314178ce87f
   SHA512 f0b1b0e1ed6e20d149d5639e14c231a46f3cdaaa6bfa320bfd64e88c4ed0496fa4246c3135e0032647c05c2aaac02804b565fbeae34b1625771114c8b263920d
 )
-vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" 
+  OPTIONS
+    -DQDRVM_BIND_CRATES=schnorrkel;bandersnatch_vrfs
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "kagome-crates")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
