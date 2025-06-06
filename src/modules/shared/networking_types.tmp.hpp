851191b2d758760b0cddb5be0806c589d468069f
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-06-06 14:12:10 +0300

    feature: networking module
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
    
    # Conflicts:
    #	src/modules/CMakeLists.txt
    #	src/se/subscription_fwd.hpp

---
+++ src/modules/shared/networking_types.tmp.hpp
@@ -0,0 +1,47 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "jam_types/types.tmp.hpp"
+#include "utils/request_id.hpp"
+
+namespace jam::messages {
+
+  struct PeerConnectedMessage {
+    PeerId peer;
+    // address?
+    // initial view?
+  };
+
+  struct PeerDisconnectedMessage {
+    PeerId peer;
+    // reason?
+  };
+
+  struct BlockAnnounce {
+    BlockHeader header;
+    PeerId peer;
+  };
+
+  struct BlockAnnounceMessage {
+    BlockAnnounce header;
+    PeerId peer;
+  };
+
+  struct BlockRequestMessage {
+    RequestCxt ctx;
+    BlocksRequest request;
+    PeerId peer;
+  };
+
+  struct BlockResponseMessage {
+    RequestCxt ctx;
+    outcome::result<Block> result;
+    PeerId peer;
+  };
+
+}
