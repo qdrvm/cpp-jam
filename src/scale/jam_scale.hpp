3fbd468a27c0d12f0ddffd1524a12c0060ec0e95
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-06-17 12:54:56 +0300

    feature: use specific jam data structure config

--- src/scale/jam_scale.hpp
+++ src/scale/jam_scale.hpp
@@ -6,19 +6,17 @@
 
 #pragma once
 
+#include <jam_types/config-tiny.hpp>
 #include <scale/scale.hpp>
 
 namespace jam {
-  using scale::impl::memory::decode;
-  using scale::impl::memory::encode;
-
   template <typename T, typename... Configs>
   [[nodiscard]] outcome::result<std::vector<uint8_t>> encode_with_config(
       T &&value, Configs &&...configs) {
     std::vector<uint8_t> out;
     scale::backend::ToBytes encoder(out, std::forward<Configs>(configs)...);
     try {
-      encode(std::forward<T>(value), encoder);
+      ::scale::encode(std::forward<T>(value), encoder);
     } catch (std::system_error &e) {
       return outcome::failure(e.code());
     }
@@ -26,16 +24,38 @@ namespace jam {
   }
 
   template <typename T, typename... Configs>
-  [[nodiscard]] outcome::result<T> decode_with_config(
-      const auto &bytes, Configs &&...configs) {
+  [[nodiscard]] outcome::result<T> decode_with_config(const auto &bytes,
+                                                      Configs &&...configs) {
     scale::backend::FromBytes decoder(bytes, std::forward<Configs>(configs)...);
     T value;
     try {
-      decode(value, decoder);
+      ::scale::decode(value, decoder);
     } catch (std::system_error &e) {
       return outcome::failure(e.code());
     }
     return std::move(value);
   }
+
+#define JAM_DATA_STRUCTURES_CONFIG tiny
+
+#ifdef JAM_DATA_STRUCTURES_CONFIG
+  template <typename T>
+  auto encode(T &&value) {
+    return encode_with_config(  //
+        std::forward<T>(value),
+        test_vectors::config::JAM_DATA_STRUCTURES_CONFIG);
+  }
+
+  template <typename T>
+  auto decode(const auto &bytes) {
+    return decode_with_config<T>(
+        std::forward<decltype(bytes)>(bytes),
+        test_vectors::config::JAM_DATA_STRUCTURES_CONFIG);
+  }
+#else
+  using scale::impl::memory::decode;
+  using scale::impl::memory::encode;
+#endif
+
 
 }  // namespace jam
