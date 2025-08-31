bf38bd358086fac208482dcbc3ecbfa826024590
turuslan <turuslan.devbox@gmail.com>
2025-08-31 23:37:34 +0500

    sszpp, leanp2p, enr
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- vcpkg-overlay/leanp2p/portfile.cmake
+++ vcpkg-overlay/leanp2p/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
-  REPO qdrvm/soralog
-  REF cf70b08f8ef86696861272bc7195242b718d23cd
-  SHA512 43f1bef886ea15de96a98a3f8aa20223e0aa8fb0f2c745b2cc2fbf368a847728602d5422a4b886c11c74a853457b35208ecef2b8f9816a1ff12b1277f854cc26
+  REPO qdrvm/leanp2p
+  REF 7cd8567a07e8b9990766c61253aabe6b0d656d04
+  SHA512 a4aa2a5a28fbe16a7ba153e49f6d6c161ae1cfbca2ec0ec04d1524efbdcb0fdb5c6d251fe06b464a534d18a44a6783eeaac085f36db3c74d53fe5fdc7b4de8c8
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
